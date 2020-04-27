39d7f453a22730575aaecdfe5c842ce598c7c4da
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2020-04-27 16:06:02 -0400

    Save data in state. Clean data function to parse json.

--- src/Screens/Calendar.js
+++ src/Screens/Calendar.js
@@ -4,18 +4,39 @@ import { AsyncStorage, View } from 'react-native';
 
 export default class CalendarTravel extends Component {
 
+    constructor(props){
+      super(props);
+      this.state = {
+        tripsData : []
+      }
+    }
+
     _retrieveData = async () => {
       try {
+        let trips = []
         const value = await AsyncStorage.getItem('@Trips');
         if (value !== null) {
-          console.log(value);
+          this.setState({
+            tripsData: value
+          })
         }
       } catch (error) {
         console.log("Error retrieving data")
       }
     };
 
-    componentDidMount(){
+    cleanData(){
+      let data = this.state.tripsData
+      let trips = []
+      data.forEach(trip => {
+        trips.push(JSON.parse(trip))
+      });
+      this.setState({
+        tripsData: trips
+      })
+    }
+
+    componentDidMount = async () => {
       this._retrieveData()
     }
 
