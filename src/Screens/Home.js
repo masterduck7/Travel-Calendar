4367d01478bf45acea244261d94b579b1d6ae1ab
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2020-05-02 13:20:07 -0400

    Confirm remove all data trip

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
-import { Image, AsyncStorage, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Modal, Image, AsyncStorage, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Card, Button } from 'react-native-elements'
 import Travel from '../../assets/Travel.png';
+import CancelledFlight from '../../assets/CancelledFlight.png';
 
 export default class Home extends Component {
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      modalRemove: false
+    }
+  }
+
   _clearData = async () => {
     try {
         await AsyncStorage.clear()
@@ -14,6 +24,28 @@ export default class Home extends Component {
   render(){
     return(
       <View style={styles.container}>
+
+        <Modal visible={this.state.modalRemove} transparent={true} animationType = {"slide"}>
+        <View style = {styles.modal}>  
+            <Card containerStyle={{width: '93%', borderRadius:20, borderWidth: 1 }}
+              title={<Text style={{textAlign:'center', paddingBottom: 15, fontSize: 20}}>¿Desea borrar todos los viajes ingresados?</Text>}>
+                <Image source={CancelledFlight} resizeMethod="resize" resizeMode="contain" alignSelf="center" style={{width:140, height:140}} /> 
+                <Text></Text>
+              <ScrollView horizontal={false}>
+                <View style={styles.buttonContainer}>
+                <Button title="SI" buttonStyle={{ backgroundColor:'#ED8C72', borderColor: '#ED8C72', borderRadius:15, borderWidth: 1, width: '80%', height: '75%', alignSelf: 'center' }} 
+                onPress = {() => {
+                  this._clearData()
+                  this.setState({modalRemove:false})
+                }}/>
+                <Button title="NO" buttonStyle={{ backgroundColor:'#2F496E', borderColor: '#2F496E', borderRadius:15, borderWidth: 1, width: '80%', height: '75%', alignSelf: 'center' }} onPress = {() => {  
+                  this.setState({ modalRemove: false })}}/>
+                </View>
+              </ScrollView>
+            </Card>
+        </View>
+        </Modal>
+
         <Text style={{color: '#888', fontSize: 50, top: -100 }}>Travel Calendar</Text>
         <Image source={Travel} resizeMethod="resize" resizeMode="stretch" style={{top: -80}} /> 
         <TouchableOpacity
@@ -27,7 +59,7 @@ export default class Home extends Component {
           <Text style={styles.text}>Añadir Viaje</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={() => this._clearData()}
+          onPress={() => this.setState({modalRemove:true})}
           style={{...styles.button, top: 30, backgroundColor: '#2F496E' }}>
           <Text style={styles.text}>Borrar datos</Text>
         </TouchableOpacity>
@@ -53,5 +85,21 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     color: '#fff'
+  },
+  modal: {  
+    justifyContent: 'center',
+    alignItems: 'center', 
+    borderColor: '#fff',    
+    marginTop: 450,
+    marginBottom: 200,
+    width: '95%',
+    paddingBottom: 15,
+    marginLeft: 10
+  },
+  buttonContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    top: 10
   }
 });
